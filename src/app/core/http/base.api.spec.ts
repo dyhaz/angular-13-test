ca5836953780a7c295eeef6ea1ddb361bdf190a2
muhammad <m.dyhaz@gmail.com>
2022-06-22 22:10:38 +0700

    enable http interceptor

--- src/app/core/http/base.api.spec.ts
+++ src/app/core/http/base.api.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, waitForAsync } from "@angular/core/testing";
 import { BaseApi, RequestMethod } from "./base.api";
-import { HTTP_INTERCEPTORS, HttpClientModule, HttpErrorResponse } from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
 import { IonicModule } from "@ionic/angular";
 import { CommonModule } from "@angular/common";
 import { firstValueFrom } from "rxjs";
@@ -39,10 +39,49 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
     const response = await firstValueFrom(app.request({
       params: {},
-      url: "https://gitlab.com/assets/",
+      url: "https://jsonplaceholder.typicode.com/todos/1",
       timeout: 1000,
       method: RequestMethod.GET
     }));
     console.log(response);
+  });
+
+  it("should request get using fetch", async () => {
+    expect(app).toBeTruthy();
+    let data = {
+      params: {},
+      url: "http://localhost:3000/dummy",
+      timeout: 1000,
+      method: RequestMethod.GET
+    };
+    let httpHeaders: HttpHeaders;
+    let headers = new HttpHeaders({ timeout: `${data?.timeout}` });
+
+    if (httpHeaders) {
+      httpHeaders.append('timeout', `${data?.timeout}`);
+    } else {
+      httpHeaders = new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: '',
+        'Access-Control-Allow-Origin': '*'
+      });
+    }
+    headers = httpHeaders;
+
+    // const httpOptions = {
+    //   headers: httpHeaders, body: data?.params
+    // };
+
+    try {
+      const response = await fetch(`${data?.url}`, {
+        headers: JSON.parse(JSON.stringify(headers)),
+        method: data?.method
+        // body: data?.params
+      });
+
+      console.log('output value:', JSON.stringify(response?.status));
+    } catch (e) {
+      // console.log('error', e);
+    }
   });
 });
